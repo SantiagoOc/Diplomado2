f4139eabe8340b13514b7e57e3998213c6d30e83
Santiago Osorio Calle <37761452-santyoc96@users.noreply.replit.com>
2024-11-22 02:18:23 +0000

    TerceraApp actualizada

--- docker/TerceraApp/app.py
+++ docker/TerceraApp/app.py
@@ -1,11 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import boto3
 
 app = Flask(__name__)
 dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
 tabla = dynamodb.Table('tabla-Santi')
 
-
+#Metodos para insertar elementos en una tabla dynamodb
 @app.route('/insert', methods=['POST'])
 def index():
   data = request.json
@@ -15,6 +15,19 @@ def index():
 
   return 'Se guardó exitosamente'
 
+#Metodos para leer elementos en una tabla dynamodb
+@app.route('/get/<id>', methods=['GET'])
+def get_item(id):
+  try:
+    response = tabla.get_item(Key={'id': id})
+
+    if 'Item' in response:
+      return jsonify(response['Item']), 200
+    else:
+      return jsonify({'error': 'id no existe'}), 404
+
+  except Exception as e:
+    return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
   app.run(debug=True, host='0.0.0.0', port=5000)
